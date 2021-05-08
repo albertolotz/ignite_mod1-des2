c740a3c2260417d13b6a7951db062d4d36727319
albertolotz <alberto10lotz@gmail.com>
2021-05-08 16:03:27 -0300

    challenge mod1-Des1 - test implemented

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,15 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "passando uma lista com números em formato de string e strings, retorna qtd de números impar" do
+      list = ["1", "3", "7", "9", "2", "22", "carro", "onibus"]
+
+      response = ListFilter.call(list)
+
+      expected_response = 4
+
+      assert response == expected_response
+    end
   end
 end
